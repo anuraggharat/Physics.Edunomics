3741eb5eaafc5ff9780d5848ac56ab302b1d7a5a
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-06 12:24:23 +0530

    main  structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Home from './Components/Home'
+import Dashboard from './Components/Dashboard'
+import Footer from './Components/Reusable/Footer'
+import Navbar from './Components/Reusable/Navbar'
 
 function App() {
   return (
     <div className="App">
-  
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home}  />
+          <Route  path="/user/dashboard" component={Dashboard}  />
+          <Route component={Error} />
+        </Switch>
+        <Footer />
+      </Router>
         
     </div>
   );
---
+++ src/Components/Reusable/Error.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Error extends Component {
+    render() {
+        return (
+            <div className="container text-center">
+                <h1>Sorry the requested page does not exist</h1>
+            </div>
+        )
+    }
+}
