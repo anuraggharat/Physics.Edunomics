ba7f40cc9f139b9923d8628df785be0d89c6a110
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-11 01:52:02 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-import Home from './Components/Home'
+import Home from './Components/Home/Home'
 import Dashboard from './Components/Dashboard'
 import Signup from './Components/Auth/Signup';
 import Login from './Components/Auth/Login';
 import SignupNew from './Components/Auth/SignupNew';
 import {Provider} from 'react-redux'
 import store from './store';
+import Error from './Components/Reusable/Error';
 
 function App() {
   return (
